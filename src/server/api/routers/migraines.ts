1918158aefbd8a4907caf0ca75b22d0903d04c39
sazquatch17 <ambush.reuse834@itsmarkg.com>
2023-04-15 16:11:25 +1000

    Added User relations to migrains

--- src/server/api/routers/migraines.ts
+++ src/server/api/routers/migraines.ts
@@ -1,9 +1,32 @@
+import type { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filerUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+}
+
 export const migrainesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.migrane.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const migraines = await ctx.prisma.migrane.findMany({
+      take: 100,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: migraines.map((migraine) => migraine.userId),
+      limit: 100,
+    })).map(filerUserForClient);
+
+    return migraines.map((migraine) => ({
+      migraine,
+      user: users.find((user) => user.id === migraine.userId),
+    }));
+
   }),
 });
